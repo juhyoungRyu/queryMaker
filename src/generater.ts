155c9dd2c6aca57e2ab4b4baf0ba89f024ce4a8f
jm.ham_demo <jm.ham_demo@quintet.co.kr>
2024-03-25 18:03:17 +0900

    edit

--- src/generater.ts
+++ src/generater.ts
@@ -9,4 +9,11 @@ export class generate extends Base{
         super();
         this.logger = new Logger("Select");
       }
+    // 필수 값 확인 user 정보, query type, table명, column명 
+    public async selectQeury(event: any) {
+        // select query에 필요한 값에 대해서 확인
+        // 쿼리 생성 확인 
+        // gpt 검증 확인 (유료)
+        // example => 생성 후 return시, 파일 생성 후 전달 화면에 뿌려주기 
+    }
 }
