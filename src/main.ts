0024a235ac6d1e72dc6a76b8cf6dd7f459446925
jh.ryu <jh.ryu@quintet.co.kr>
2024-03-25 18:05:39 +0900

    feat. koa 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import Koa from "koa";
+import Koa_Router from "@koa/router";
+
 // util
 import LogUtil from "./util/LogUtil";
 import CliUtil from "./util/CliUil";
@@ -10,32 +13,39 @@ const Logger = new LogUtil("main");
 const Cli = new CliUtil();
 const Sheet = new SheetUtil();
 
-// cli 방식으로 이후 기능 추가
-/* makeQuery
- * 1. excel 기반 table 생성 쿼리
+const Server = new Koa();
+const Router = new Koa_Router();
+
+/* makeQuery : cli 기반 동작
+ * 1. excel 기반 CREATE TABLE 쿼리 생성
  */
+// async function makeQuery() {
+//   const result = await Cli.question(
+//     "text",
+//     "Please input the file path of your table blueprint (Absolute Path) : "
+//   );
 
-async function makeQuery() {
-  const result = await Cli.question(
-    "text",
-    "Please input the file path of your table blueprint (Absolute Path) : "
-  );
+//   if (!result.success || (result.success && result.path === "")) {
+//     Logger.log("error", `The file does not exist in that path.`);
+//     return;
+//   }
 
-  if (!result.success || (result.success && result.path === "")) {
-    Logger.log("error", `The file does not exist in that path.`);
-    return;
-  }
+//   Logger.log("info", `File Path : ${result.path}`);
+//   Logger.log("line");
+//   Sheet.setSheetData(result.path);
 
-  Logger.log("info", `File Path : ${result.path}`);
-  Logger.log("line");
-  Sheet.setSheetData(result.path);
-
-  // sheet 데이터
-  const objSheet = Sheet.getSheetData();
-  // 테이블명 배열
-  const arrTableName = Object.keys(objSheet);
-}
+//   // sheet 데이터
+//   const objSheet = Sheet.getSheetData();
+//   // 테이블명 배열
+//   const arrTableName = Object.keys(objSheet);
+// }
 
 async function createTableQuery(tableName: string, tableData: TableInfo) {}
 
-makeQuery();
+Router.get("/", (ctx) => {
+  ctx.body = "Hello, World";
+});
+
+Server.use(Router.routes()).use(Router.allowedMethods());
+
+Server.listen(3000);
