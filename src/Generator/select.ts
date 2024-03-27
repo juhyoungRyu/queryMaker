4411af15e580b56361a8a85c21cd96b4a3ca0f22
jm.ham_demo <jm.ham_demo@quintet.co.kr>
2024-03-27 09:21:16 +0900

    merge

--- src/Generator/select.ts
+++ src/Generator/select.ts
@@ -19,27 +19,17 @@ export default class CreateGenerator extends Base {
   public async selectQeury(event: any) {
     const body: any = event.body 
 
-    if(this._.isEmpty(body.table) || body.hasOwnProperty(body,'table')){ // table name 
+    if(this._.isEmpty(body.table) || !body.table){ // table name 
         body.response.status = 401;
         body.response.message = "table";
         return;
     }
 
-    if(this._.isEmpty(body.column)|| body.hasOwnProperty(body,'column')){ // type 
+    if(this._.isEmpty(body.column)|| body.column){ // type 
         body.response.status = 401;
         body.response.message = "column";
         return;
     }
-    // SELECT, COLUMN, FROM tableName WHERE ORDER BY 
-
-    // 필수로 필요한 값 : Column, tableName 
-
-    // 항상 작성되어야하는 부분: SELECT, FROM, WHERE 1 = 1 
-
-    // 조건으로 붙어야할 항목 : 
-        // WHERE에는 AND COLUMN 조건식 조건 array index에 ''을 기준? 아님 key: value형태로 받을지 확인 
-        // ORDERT BY column array index에 ''을 기준? 아님 key: value형태로 받을지 확인 
-    // column 객체에서 SQL 컬럼 문자열 생성
     const columnsArray = Object.entries(body.column).map(([key, value]) => {
         return `${key} AS ${value}`;
     });
