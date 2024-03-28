3bd6d8b9570090672420d81dad09c72fa53b6dbc
jh.ryu <jh.ryu@quintet.co.kr>
2024-03-28 14:50:09 +0900

    pnpm -> npm (cloudtype)

--- build/main.js
+++ build/main.js
@@ -15,13 +15,13 @@ const TableRouter_1 = require("./Router/TableRouter");
 const logUtil_1 = __importDefault(require("./util/logUtil"));
 // init
 const Server = new koa_1.default();
-const Router = new BaseRouter_1.default();
+const SystemRouter = new BaseRouter_1.default().API;
 const Logger = new logUtil_1.default("main");
-Router.API.use("/select", (0, SelectRouter_1.SelectRouter)().API.routes());
-Router.API.use("/table", (0, TableRouter_1.TableRouter)().API.routes());
+SystemRouter.use("/select", (0, SelectRouter_1.SelectRouter)().API.routes());
+SystemRouter.use("/table", (0, TableRouter_1.TableRouter)().API.routes());
 // Server Module
 Server.use((0, cors_1.default)());
 Server.use((0, koa_bodyparser_1.default)());
-Server.use(Router.API.routes()).use(Router.API.allowedMethods());
+Server.use(SystemRouter.routes()).use(SystemRouter.allowedMethods());
 // Server Start
 Server.listen(3001, () => Logger.log("info", "🚀 Server Start 🚀"));
