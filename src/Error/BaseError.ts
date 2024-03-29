9780144198e3283b1bf27bae8ae71407c8a045f3
jh.ryu <jh.ryu@quintet.co.kr>
2024-03-29 09:48:01 +0900

    error 초안

--- src/Error/BaseError.ts
+++ src/Error/BaseError.ts
@@ -3,6 +3,7 @@ import Logger from "../util/logUtil";
 
 export default class BaseError extends Base {
   protected Logger: Logger;
+
   constructor(exceptionName: string) {
     super();
     this.Logger = new Logger(exceptionName);
